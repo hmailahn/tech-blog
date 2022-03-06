5e1fd54bf623dab6cb6d9db54411dd0b2c3d99fe
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-03-06 14:08:48 -0600

    adding timeout to 30000

--- server.js
+++ server.js
@@ -37,11 +37,3 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
-
-// THEN I am taken to the homepage and presented with existing blog posts that include the post title and the date created
-// WHEN I click on an existing blog post
-// THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
-
-
-// WHEN I am idle on the site for more than a set time
-// THEN I am able to view comments but I am prompted to log in again before I can add, update, or delete comments
